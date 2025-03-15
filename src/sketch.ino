c4e709265869aeaddcd24137a11e8aad509c6d29
M.K. Akindu Kalhan <kalhanmka.22@uom.lk>
2025-03-15 18:33:59 +0530

    Move sketch.ino to src directory and implement time display functionality

--- src/sketch.ino
+++ src/sketch.ino
@@ -9,6 +9,11 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH,SCREEN_HEIGHT,&Wire,OLED_RESET);
 
+int days = 0;
+int hours = 0;
+int minutes = 0;
+int seconds = 0;
+
 void setup(){
     Serial.begin(9600);
 
@@ -22,23 +27,30 @@ void setup(){
     display.clearDisplay();
 
     print_line("Welcome to Medobox", 10, 20, 2);
+    display.clearDisplay();
 }
 
 void loop(){
-
+    print_time_now();
 }
 
 void print_line(String text, int text_size, int row, int column){
 
-    display.clearDisplay();
     display.setTextSize(text_size);
     display.setTextColor(SSD1306_WHITE);
     display.setCursor(row,column);
     display.println(text);
-
     display.display();
-    delay(2000);
+}
 
+void print_time_now(void){
+    print_line(String(days), 0, 0, 2);
+    print_line(":", 20, 0, 2);
+    print_line(String(hours), 30, 0, 2);
+    print_line(":", 50, 0, 2);
+    print_line(String(minutes), 60, 0, 2);
+    print_line(":", 80, 0, 2);
+    print_line(String(seconds), 90, 0, 2);
 }
 
 
